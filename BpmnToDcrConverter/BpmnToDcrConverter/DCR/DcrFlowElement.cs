f00afb1c2f3323de8908dde10d6c0ff609f21abe
Daniel Ellebæk Nielsen <daniogdan@gmail.com>
2023-08-31 00:15:04 +0200

    Started adding DCR graph definitions

---
+++ BpmnToDcrConverter/BpmnToDcrConverter/DCR/DcrFlowElement.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace BpmnToDcrConverter.DCR
+{
+    public abstract class DcrFlowElement
+    {
+        public int Id;
+    }
+
+    public class DcrActivity : DcrFlowElement
+    {
+        public string Name;
+
+        public bool Included;
+        public bool Executed;
+        public bool Pending;
+
+        public DcrActivity(string name, bool included, bool executed, bool pending)
+        {
+            Name = name;
+
+            Included = included;
+            Executed = executed;
+            Pending = pending;
+        }
+
+        public DcrActivity(string name) : this(name, true, false, false) { }
+    }
+
+    public class DcrGroup : DcrFlowElement
+    {
+        public string Name;
+        public List<DcrFlowElement> Activities;
+
+        public DcrGroup(string name, IEnumerable<DcrFlowElement> activities)
+        {
+            Name = name;
+            Activities = activities.ToList();
+        }
+    }
+}
