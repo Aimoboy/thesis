8066ea7765d5cc4c83f6a70bc628bac0567d8d83
Daniel Ellebæk Nielsen <dn@dalux.com>
2023-08-30 16:22:42 +0200

    Changed project to C# in visual studio

---
+++ BpmnToDcrConverter/BpmnToDcrConverter/BPMN/BpmnFlowElement.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace BpmnToDcrConverter.BPMN
+{
+    public abstract class BpmnFlowElement
+    {
+        public int Id;
+    }
+
+    public class BpmnActivity : BpmnFlowElement
+    {
+        public string Name;
+
+        public BpmnActivity(int id, string name)
+        {
+            Id = id;
+            Name = name;
+        }
+    }
+
+    public class BpmnEvent : BpmnFlowElement
+    {
+        public BpmnEventType Type;
+
+        public BpmnEvent(int id, BpmnEventType type)
+        {
+            Id = id;
+            Type = type;
+        }
+    }
+
+    public class BpmnGateway : BpmnFlowElement
+    {
+        public BpmnGatewayType Type;
+
+        public BpmnGateway(int id, BpmnGatewayType type)
+        {
+            Id = id;
+            Type = type;
+        }
+    }
+
+    public enum BpmnEventType
+    {
+        Start,
+        End
+    }
+
+    public enum BpmnGatewayType
+    {
+        Or,
+        And
+    }
+}
---
+++ BpmnToDcrConverter/BpmnToDcrConverter/BPMN/BpmnGraph.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace BpmnToDcrConverter.BPMN
+{
+    public class BpmnGraph
+    {
+
+    }
+}
