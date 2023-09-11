da8da059af30e3997306487a2cf56bd3e4a7af66
Daniel Ellebæk Nielsen <daniogdan@gmail.com>
2023-09-12 00:13:30 +0200

    Added start event, end event and gateway arrow unit tests

--- BpmnToDcrConverter/UnitTests/Bpmn/BpmnInvalidArrowTests.cs
+++ BpmnToDcrConverter/UnitTests/Bpmn/BpmnInvalidArrowTests.cs
@@ -66,5 +66,80 @@ namespace UnitTests.Bpmn
 
             activity3.TestArrowCountValidity();
         }
+
+        [TestMethod]
+        [ExpectedException(typeof(BpmnInvalidArrowException))]
+        public void InvalidArrowStartEventAnyIncoming()
+        {
+            BpmnEvent start = new BpmnEvent("1", BpmnEventType.Start);
+            BpmnActivity activity = new BpmnActivity("2", "Activity!");
+
+            BpmnGraph graph = new BpmnGraph(new BpmnFlowElement[] { start, activity });
+            graph.AddArrow(BpmnFlowArrowType.Sequence, activity, start);
+
+            start.TestArrowCountValidity();
+        }
+
+        [TestMethod]
+        [ExpectedException(typeof(BpmnInvalidArrowException))]
+        public void InvalidArrowStartEventTooManyOutgoing()
+        {
+            BpmnEvent start = new BpmnEvent("1", BpmnEventType.Start);
+            BpmnActivity activity1 = new BpmnActivity("2", "Activity!");
+            BpmnActivity activity2 = new BpmnActivity("3", "Activity!");
+
+            BpmnGraph graph = new BpmnGraph(new BpmnFlowElement[] { start, activity1, activity2 });
+            graph.AddArrow(BpmnFlowArrowType.Sequence, start, activity1);
+            graph.AddArrow(BpmnFlowArrowType.Sequence, start, activity2);
+
+            start.TestArrowCountValidity();
+        }
+
+        [TestMethod]
+        [ExpectedException(typeof(BpmnInvalidArrowException))]
+        public void InvalidArrowEndEventAnyOutgoing()
+        {
+            BpmnEvent end = new BpmnEvent("1", BpmnEventType.End);
+            BpmnActivity activity = new BpmnActivity("2", "Activity!");
+
+            BpmnGraph graph = new BpmnGraph(new BpmnFlowElement[] { end, activity });
+            graph.AddArrow(BpmnFlowArrowType.Sequence, end, activity);
+
+            end.TestArrowCountValidity();
+        }
+
+        [TestMethod]
+        [ExpectedException(typeof(BpmnInvalidArrowException))]
+        public void InvalidArrowEndEventNoIncoming()
+        {
+            BpmnEvent end = new BpmnEvent("1", BpmnEventType.End);
+            end.TestArrowCountValidity();
+        }
+
+        [TestMethod]
+        [ExpectedException(typeof(BpmnInvalidArrowException))]
+        public void InvalidArrowGatewayNoOutgoing()
+        {
+            BpmnGateway gateway = new BpmnGateway("1", BpmnGatewayType.Or);
+            BpmnActivity activity = new BpmnActivity("2", "Activity!");
+
+            BpmnGraph graph = new BpmnGraph(new BpmnFlowElement[] { gateway, activity });
+            graph.AddArrow(BpmnFlowArrowType.Sequence, activity, gateway);
+
+            gateway.TestArrowCountValidity();
+        }
+
+        [TestMethod]
+        [ExpectedException(typeof(BpmnInvalidArrowException))]
+        public void InvalidArrowGatewayNoIncoming()
+        {
+            BpmnGateway gateway = new BpmnGateway("1", BpmnGatewayType.Or);
+            BpmnActivity activity = new BpmnActivity("2", "Activity!");
+
+            BpmnGraph graph = new BpmnGraph(new BpmnFlowElement[] { gateway, activity });
+            graph.AddArrow(BpmnFlowArrowType.Sequence, gateway, activity);
+
+            gateway.TestArrowCountValidity();
+        }
     }
 }
