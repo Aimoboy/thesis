d1b825bcd23c20ccb2c25ad366c64ddf00a5eb7c
Daniel Ellebæk Nielsen <dn@dalux.com>
2023-10-23 16:32:01 +0200

    Simple unit tests for parsing constants

---
+++ BpmnToDcrConverter/UnitTests/ConditionParsing/RelationalExpressionParsing.cs
@@ -0,0 +1,27 @@
+﻿using Sprache;
+using BpmnToDcrConverter;
+
+namespace UnitTests.ConditionParsing
+{
+    [TestClass]
+    public class RelationalExpressionParsing
+    {
+        [TestMethod]
+        public void Integers()
+        {
+            Expression exp = LogicParser.ConditionParser.Parse("1 < 2");
+            Expression trueExp = new RelationalOperation(new IntegerConstant(1), RelationalOperator.LessThan, new IntegerConstant(2));
+
+            Assert.IsTrue(exp.EqualToExpression(trueExp));
+        }
+
+        [TestMethod]
+        public void Decimals()
+        {
+            Expression exp = LogicParser.ConditionParser.Parse("0.0065 < 0.008");
+            Expression trueExp = new RelationalOperation(new DecimalConstant(0.0065m), RelationalOperator.LessThan, new DecimalConstant(0.008m));
+
+            Assert.IsTrue(exp.EqualToExpression(trueExp));
+        }
+    }
+}
